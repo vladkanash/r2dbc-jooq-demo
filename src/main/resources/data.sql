02cb2e00cbbe02b72cdd545ba1fb885460e63c1d
Vlad Kanash <vlad.kanash@gmail.com>
2023-12-16 18:09:40 +0100

    add orders table

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,13 @@ INSERT INTO products (id, name, quantity) VALUES ('2', 'Iphone', 2143);
 INSERT INTO products (id, name, quantity) VALUES ('3', 'Xbox Series X', 2143);
 INSERT INTO products (id, name, quantity) VALUES ('4', 'Steam Deck', 245345);
 INSERT INTO products (id, name, quantity) VALUES ('5', 'PS 5', 2143);
+
+INSERT INTO orders (id) VALUES ('714ff144-612c-4cff-90d3-990fabb80fee');
+INSERT INTO orders (id) VALUES ('54a94fe7-14f5-4075-bb21-761a63309a0e');
+INSERT INTO orders (id) VALUES ('273ab9ec-eca5-4844-967b-47463db94da5');
+INSERT INTO orders (id) VALUES ('f2528662-fbbd-48e9-a4cc-4c35e43ef0de');
+INSERT INTO orders (id) VALUES ('67a4ac6e-2bee-4222-a5d4-89d0b141aea6');
+
+INSERT INTO order_items (order_id, product_id) VALUES ('714ff144-612c-4cff-90d3-990fabb80fee', '1');
+INSERT INTO order_items (order_id, product_id) VALUES ('714ff144-612c-4cff-90d3-990fabb80fee', '3');
+INSERT INTO order_items (order_id, product_id) VALUES ('714ff144-612c-4cff-90d3-990fabb80fee', '5');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,3 +3,12 @@ CREATE TABLE products (
     name text not null,
     quantity int not null
 );
+
+CREATE TABLE orders (
+    id text not null primary key
+);
+
+CREATE TABLE order_items (
+    order_id text not null references orders(id),
+    product_id text not null references products(id)
+);
